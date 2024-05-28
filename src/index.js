2afbd67125f6829b4db210a451beaabcddbc02ba
Jacobo <tadeo.jacobo6@gmail.com>
2024-05-28 15:07:30 -0400

    connect db

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const { MONGODB_URI } = process.env;
 const app = express();
 const port = 3000;
 
+// Routes
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+// Connect to MongoDB
+mongoose.connect(MONGODB_URI)
+.then(() => console.log('Connected to MongoDB'))
+.catch((err) => console.error(err));
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
